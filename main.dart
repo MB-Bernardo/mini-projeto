11ee7f6275bfe6a5857c1f8ac530473547aae6ed
Bernardo Netto <bernardodonetto@gmail.com>
2026-05-27 20:52:28 -0300

    feat: class-tarefa factory e funcoes

---
+++ main.dart
@@ -0,0 +1,55 @@
+import 'dados_tarefa.dart';
+import 'models.dart';
+
+
+void main(){
+  List<Tarefa> tarefas = [];
+  for (var item in dadosTarefas) {
+    Tarefa tarefa = Tarefa.fromMap(item);
+
+    tarefas.add(tarefa);
+  }
+
+  for (var tarefa in tarefas){
+    tarefa.exibirResumo();
+  }
+
+  List<Tarefa> tarefasConcluidas = 
+  tarefas.where((tarefa){
+    return tarefa.status == 'concluida';
+  }).toList();
+
+  print('Tarefas concluidas:');
+
+  for(var tarefa in tarefasConcluidas){
+    tarefa.exibirResumo();
+  }
+
+  double somaValores = 0;
+  for(var tarefa in tarefasConcluidas){
+    somaValores += tarefa.valor;
+  }
+  print('Valor total das tarefas concluidas:');
+  print('R\$ $somaValores');
+
+  List<Tarefa> tarefasPendentes = 
+  tarefas.where((tarefa){
+    return tarefa.status == 'pendente';
+  }).toList();
+  double somaPendente = 0;
+  for(var tarefa in tarefasPendentes){
+    somaPendente += tarefa.valor;
+    }
+
+  double media = somaPendente / tarefasPendentes.length;
+  
+
+
+
+  print('');
+  print('Media das tarefas pendentes:');
+  print(media);
+
+
+
+}
---
+++ models.dart
@@ -0,0 +1,70 @@
+ import 'funcoes.dart';
+
+
+class ItemTrabalho {
+  int id;
+  String titulo;
+
+
+
+  ItemTrabalho({
+    required this.id,
+    required this.titulo,
+  });
+
+  void exibirResumo() {
+    print('Item $id - $titulo');
+  }
+}
+
+class Tarefa extends ItemTrabalho {
+
+
+  String responsavel;
+  String status;
+  String prioridade;
+  double valor;
+  int horas;
+
+  Tarefa({
+    required int id,
+    required String titulo,
+    required this.responsavel,
+    required this.status,
+    required this.prioridade,
+    required this.valor,
+    required this.horas,
+  }) : super(
+         id: id,
+        titulo: titulo,
+    );
+
+    factory Tarefa.fromMap(Map<String, dynamic> item) {
+    return Tarefa(
+      id: item['id'] ?? 0,
+      titulo: item['titulo'] ?? 'Sem titulo',
+      responsavel: item['responsavel'] ?? 'Não informado',
+      status: item['status'] ?? 'Sem status',
+      prioridade: item['prioridade'] ?? 'Sem prioridade',
+
+
+      valor: tratarValores(item['valor']),
+      horas: tratarHoras(item['horas']),
+    );
+
+
+
+  }
+
+    
+
+    
+
+  @override
+  void exibirResumo(){
+    print('Tarefa $id - $titulo | Status : $status | Valor: R\$ $valor',);
+  }
+  
+
+ 
+}
